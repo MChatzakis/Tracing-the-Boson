c110999a062e9b0b2ee966c7a4dd02344b6991d6
maxencehof <114079356+maxencehof@users.noreply.github.com>
2022-10-20 14:23:57 +0200

    Helpers with 3 functions

--- src/maxence/helpers.py
+++ src/maxence/helpers.py
@@ -1,69 +1,91 @@
-# -*- coding: utf-8 -*-
-"""some helper functions."""
+"""Some helper functions for project 1."""
+import csv
 import numpy as np
 
 
-def load_and_build_data():
-    """Load data and convert it to the metric system."""
-    path_dataset = "INSERT_PATH\\data\\train.csv" #don't forget double backslash
-    data = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1)
-    y_bs = np.genfromtxt(
-        path_dataset, delimiter=",", skip_header=1, dtype=None, encoding=None, usecols=(1))
-    x = np.asarray(data[:, 2:])
-    #print(data[1,:])
-    y = np.zeros(len(y_bs))
-    for i in range(0, len(y_bs)):
-        if y_bs[i] == 'b':
-            y[i]=1
-        elif y_bs[i]== 's':
-            y[i]=-1
+def load_csv_data(data_path, sub_sample=False):
+    """Loads data and returns y (class labels), tX (features) and ids (event ids)"""
+    y = np.genfromtxt(data_path, delimiter=",", skip_header=1, dtype=str, usecols=1)
+    x = np.genfromtxt(data_path, delimiter=",", skip_header=1)
+    ids = x[:, 0].astype(np.int)
+    input_data = x[:, 2:]
+
+    # convert class labels from strings to binary (-1,1)
+    yb = np.ones(len(y))
+    yb[np.where(y == "b")] = -1
+
+    # sub-sample
+    if sub_sample:
+        yb = yb[::50]
+        input_data = input_data[::50]
+        ids = ids[::50]
+
+    return yb, input_data, ids
+
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    Creates an output file in .csv format for submission to Kaggle or AIcrowd
+    Arguments: ids (event ids associated with each prediction)
+               y_pred (predicted class labels)
+               name (string name of .csv output file to be created)
+    """
+    with open(name, "w") as csvfile:
+        fieldnames = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
+
+
+def remove_false_lines(y, x):
+    ''' this function take y and x and remove the data with the untaked measurement (-999.0)
+    Input: y: shape (n, )
+           x: shape (n, d)
+
+    Return: ry: shape (nr, ) 
+            rx: shape (nr, d) The data without lines containing -999
+            n999: shape (1, d) The number of -999 per column
+    '''
+
+    [n, d] = np.shape(x)
+    n999=np.zeros(d)
+    index=[-1]
+    for i in range(0, n):
+        if not(-999 in x[i, :]):
+            index=np.vstack([index, i])
         else:
-            y[i]=-999
-    
+            for j in range(0, d):
+                if (x[i, j]==-999):
+                    n999[j]=n999[j]+1
 
-    return y, x
+    index=np.delete(index, 0)
 
+    rx=x[index, :]
+    ry=y[index]
+    return ry, rx, n999
 
-def standardize(x):
-    """Standardize the original data set."""
-    mean_x = np.mean(x)
-    x = x - mean_x
-    std_x = np.std(x)
-    x = x / std_x
-    return x, mean_x, std_x
+def remove_false_columns(x):
+    ''' this function take x and remove the data with the untaked measurement (-999.0)
+    Input: x: shape (n, d)
 
+    Return: rx: shape (nr, d) The data without columns containing -999
+    '''
+    [n, d] = np.shape(x)
+    n999=np.zeros(d)
+    index=[-1]
+    for i in range(0, d):
+        if not(-999 in x[:, i]):
+            index=np.vstack([index, i])
 
-def build_model_data(height, weight):
-    """Form (y,tX) to get regression data in matrix form."""
-    y = weight
-    x = height
-    num_samples = len(y)
-    tx = np.c_[np.ones(num_samples), x]
-    return y, tx
+    index=np.delete(index, 0)
 
+    rx=x[:, index]
+    return rx
 
-def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
-    """
-    Generate a minibatch iterator for a dataset.
-    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
-    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
-    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
-    Example of use :
-    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
-        <DO-SOMETHING>
-    """
-    data_size = len(y)
-
-    if shuffle:
-        shuffle_indices = np.random.permutation(np.arange(data_size))
-        shuffled_y = y[shuffle_indices]
-        shuffled_tx = tx[shuffle_indices]
-    else:
-        shuffled_y = y
-        shuffled_tx = tx
-    for batch_num in range(num_batches):
-        start_index = batch_num * batch_size
-        end_index = min((batch_num + 1) * batch_size, data_size)
-        if start_index != end_index:
-            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+def add_1_column(x):
+    ''' Take a x vector and add a 1 column at the beginning for the w0 element
+    '''
+    [n, d] = np.shape(x)
+    v=np.ones([n, 1])
+    return np.hstack((v, x))
