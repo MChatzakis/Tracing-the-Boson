4e467c0c3251e4993028ddac4fdf23b4a16513d4
Manolis Chatzakis <chatzakis@ics.forth.gr>
2022-10-21 23:41:01 +0200

    Update implementations.py

--- src/implementations.py
+++ src/implementations.py
@@ -92,23 +92,26 @@ def least_squares(y, tx):
 
 def ridge_regression(y, tx, lambda_):
     """implement ridge regression.
-
+    
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
         lambda_: scalar.
-
+    
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-
+        mse: scalar
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
     array([ 0.21212121, -0.12121212])
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
     array([0.03947092, 0.00319628])
     """
-    w = np.linalg.solve(tx.T.dot(tx) + lambda_*2 *
-                        tx.shape[0]*np.identity(tx.shape[1]), tx.T.dot(y))
-    return w
+    w = np.linalg.solve(tx.T.dot(tx) + lambda_*2*tx.shape[0]*np.identity(tx.shape[1]), tx.T.dot(y))
+    error = y - tx.dot(w)
+    mse = 1/(2*y.shape[0])*np.transpose(error).dot(error)
+    
+    return w, mse
+
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
