dcb79a575f3e559b45c3241e47b817e506952eb0
yannisban <johnbantzis@gmail.com>
2022-10-22 13:09:29 +0200

    minor changes in cross-validation

--- src/giannis/validation.py
+++ src/giannis/validation.py
@@ -101,20 +101,54 @@ def split_data_k_fold(x,y, k_fold, k, seed):
     
 #     return loss_tr, loss_te
 
-def cross_validation_step(x,y, k_fold, k, seed,function,*args, loss_function):
+def cross_validation_step(x,y, k_fold, k, seed,function, loss_function,*args):
     x_te, x_tr, y_te, y_tr = split_data_k_fold(x,y, k_fold, k, seed)
-    loss_tr, w = function(y_tr, x_tr, *args)
+    w, loss_tr  = function(y_tr, x_tr, *args)
     loss_te = loss_function(y_te, x_te, w)
     
     return loss_tr, loss_te
 
-def cross_validation(x,y, k_fold, k, seed,function,*args, loss_function):
+def cross_validation(x,y, k_fold, seed,function, loss_function,*args):
     losses_tr = []
     losses_te = []
     for k in range(k_fold):
-        k_loss_tr,k_loss_te = cross_validation_step(x,y, k_fold, k, seed,function,*args, loss_function)
+        k_loss_tr,k_loss_te = cross_validation_step(x,y, k_fold, k, seed,function, loss_function,*args)
         losses_tr.append(k_loss_tr)
         losses_te.append(k_loss_te)
     loss_tr = np.mean(losses_tr)
     loss_te = np.mean(losses_te) 
     return loss_te, loss_tr   
+
+def split_data(x, y, ratio, seed=1):
+    """
+    split the dataset based on the split ratio. If ratio is 0.8 
+    you will have 80% of your data set dedicated to training 
+    and the rest dedicated to testing. If ratio times the number of samples is not round
+    you can use np.floor. Also check the documentation for np.random.permutation,
+    it could be useful.
+    
+    Args:
+        x: numpy array of shape (N,), N is the number of samples.
+        y: numpy array of shape (N,).
+        ratio: scalar in [0,1]
+        seed: integer.
+        
+    Returns:
+        x_tr: numpy array containing the train data.
+        x_te: numpy array containing the test data.
+        y_tr: numpy array containing the train labels.
+        y_te: numpy array containing the test labels.
+        
+    >>> split_data(np.arange(13), np.arange(13), 0.8, 1)
+    (array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]), array([ 2,  3,  4, 10,  1,  6,  0,  7, 12,  9]), array([ 8, 11,  5]))
+    """
+    # set seed
+    np.random.seed(seed)
+
+    p = np.random.permutation(x.shape[0])
+    x, y = x[p], y[p]
+    x_tr = x[:int(np.floor(ratio*x.shape[0]))]
+    x_te = x[int(np.floor(ratio*x.shape[0])):]
+    y_tr = y[:int(np.floor(ratio*y.shape[0]))]
+    y_te = y[int(np.floor(ratio*y.shape[0])):]
+    return x_tr, x_te, y_tr, y_te
