390fe68c87fbc49e04760d023bb605b28036c751
yannisban <johnbantzis@gmail.com>
2022-10-29 16:54:07 +0200

    improve function documentation

--- implementations.py
+++ implementations.py
@@ -10,9 +10,6 @@ Ioannis Bantzis, Manos Chatzakis, Maxence Hofer
 
 
 import numpy as np
-
-from cmath import inf
-
 from utils import *
 from helpers import *
 
@@ -21,15 +18,15 @@ def mean_squared_error_gd(y, tx, initial_w, max_iters, gamma):
     """The Gradient Descent (GD) algorithm.
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,D)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        max_iters: a scalar denoting the total number of iterations of GD
-        gamma: a scalar denoting the stepsize
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of GD.
+        gamma: a scalar denoting the stepsize.
 
     Returns:
-        loss: The loss of the final computation [value]
-        w: The final weights [numpy array of D dimensions]
+        loss: The loss of the final computation [scalar].
+        w: The final weights [numpy array of shape=(D,)].
     """
 
     loss = -1
@@ -47,16 +44,15 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
     """The Stochastic Gradient Descent algorithm (SGD).
 
     Args:
-        y: numpy array of shape=(N, )
-        tx: numpy array of shape=(N,2)
-        initial_w: numpy array of shape=(2, ). The initial guess (or the initialization) for the model parameters
-        batch_size: a scalar denoting the number of data points in a mini-batch used for computing the stochastic gradient
-        max_iters: a scalar denoting the total number of iterations of SGD
-        gamma: a scalar denoting the stepsize
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of SGD.
+        gamma: a scalar denoting the stepsize.
 
     Returns:
-        losses: a list of length max_iters containing the loss value (scalar) for each iteration of SGD
-        ws: a list of length max_iters containing the model parameters as numpy arrays of shape (2, ), for each iteration of SGD 
+        loss: The loss of the final computation [scalar]
+        w: The final weights [numpy array of shape=(D,)]
     """
     batch_size = 1
     loss = -1
@@ -76,8 +72,7 @@ def mean_squared_error_sgd(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares(y, tx):
-    """Calculate the least squares solution.
-       returns mse, and optimal weights.
+    """ The Least Squares Algorith (LS).
 
     Args:
         y: numpy array of shape (N,), N is the number of samples.
@@ -85,7 +80,7 @@ def least_squares(y, tx):
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar.
+        mse: The MSE loss of the final computation [scalar].
 
     """
     # ***************************************************
@@ -99,16 +94,17 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression.
+    """ The Ridge Regression Algorithm (RR).
 
     Args:
         y: numpy array of shape (N,), N is the number of samples.
         tx: numpy array of shape (N,D), D is the number of features.
-        lambda_: scalar.
+        lambda_: a scalar denoting the regularization parameter.
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar
+        mse: The MSE loss of the final computation [scalar].
+
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 0)
     array([ 0.21212121, -0.12121212])
     >>> ridge_regression(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), 1)
@@ -123,16 +119,18 @@ def ridge_regression(y, tx, lambda_):
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """Calculate the least squares solution.
-       returns mse, and optimal weights.
+    """The Logistic Regression Algorithm (LR). 
 
     Args:
-        y: numpy array of shape (N,), N is the number of samples.
-        tx: numpy array of shape (N,D), D is the number of features.
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of LR.
+        gamma: a scalar denoting the stepsize.
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar.
+        loss: The logistic loss of the final computation [scalar].
 
     """
     loss = -1
@@ -146,16 +144,19 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
 
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma_):
-    """Calculate the least squares solution.
-       returns mse, and optimal weights.
+    """ The Regularized Logistic Regression Algorithm (LRL).
 
     Args:
-        y: numpy array of shape (N,), N is the number of samples.
-        tx: numpy array of shape (N,D), D is the number of features.
+        y: numpy array of shape=(N, ).
+        tx: numpy array of shape=(N,D).
+        lambda_: a scalar denoting the regularization parameter.
+        initial_w: numpy array of shape=(D, ). The initial guess (or the initialization) for the model parameters.
+        max_iters: a scalar denoting the total number of iterations of SGD.
+        gamma_: a scalar denoting the stepsize.
 
     Returns:
         w: optimal weights, numpy array of shape(D,), D is the number of features.
-        mse: scalar.
+        loss: The logistic loss of the final computation [scalar].
 
     """
 
